12cf074caa095f1dd1d1c31da4e4a18553a0de36
Fachry Dennis Heraldi <71638224+dennisheraldi@users.noreply.github.com>
2023-03-24 04:43:20 +0700

    feat: normal vector calculation

--- src/utils/mat.js
+++ src/utils/mat.js
@@ -25,11 +25,23 @@ var m4 = {
         var cosBeta = Math.cos(beta);
         var sinBeta = Math.sin(beta);
         return [
-            1, 0, 0, 0,
-            0, 1, 0, 0,
-            f * cosBeta, f * sinBeta, 1, 0,
-            0, 0, 0, 1,
-        ]
+            1,
+            0,
+            0,
+            0,
+            0,
+            1,
+            0,
+            0,
+            f * cosBeta,
+            f * sinBeta,
+            1,
+            0,
+            0,
+            0,
+            0,
+            1,
+        ];
     },
 
     multiply: function (a, b) {
@@ -167,65 +179,65 @@ var m4 = {
             d * t2,
             d * t3,
             d *
-            (tmp_1 * m10 +
-                tmp_2 * m20 +
-                tmp_5 * m30 -
-                (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30)),
+                (tmp_1 * m10 +
+                    tmp_2 * m20 +
+                    tmp_5 * m30 -
+                    (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30)),
             d *
-            (tmp_0 * m00 +
-                tmp_7 * m20 +
-                tmp_8 * m30 -
-                (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30)),
+                (tmp_0 * m00 +
+                    tmp_7 * m20 +
+                    tmp_8 * m30 -
+                    (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30)),
             d *
-            (tmp_3 * m00 +
-                tmp_6 * m10 +
-                tmp_11 * m30 -
-                (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30)),
+                (tmp_3 * m00 +
+                    tmp_6 * m10 +
+                    tmp_11 * m30 -
+                    (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30)),
             d *
-            (tmp_4 * m00 +
-                tmp_9 * m10 +
-                tmp_10 * m20 -
-                (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20)),
+                (tmp_4 * m00 +
+                    tmp_9 * m10 +
+                    tmp_10 * m20 -
+                    (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20)),
             d *
-            (tmp_12 * m13 +
-                tmp_15 * m23 +
-                tmp_16 * m33 -
-                (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33)),
+                (tmp_12 * m13 +
+                    tmp_15 * m23 +
+                    tmp_16 * m33 -
+                    (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33)),
             d *
-            (tmp_13 * m03 +
-                tmp_18 * m23 +
-                tmp_21 * m33 -
-                (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33)),
+                (tmp_13 * m03 +
+                    tmp_18 * m23 +
+                    tmp_21 * m33 -
+                    (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33)),
             d *
-            (tmp_14 * m03 +
-                tmp_19 * m13 +
-                tmp_22 * m33 -
-                (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33)),
+                (tmp_14 * m03 +
+                    tmp_19 * m13 +
+                    tmp_22 * m33 -
+                    (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33)),
             d *
-            (tmp_17 * m03 +
-                tmp_20 * m13 +
-                tmp_23 * m23 -
-                (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23)),
+                (tmp_17 * m03 +
+                    tmp_20 * m13 +
+                    tmp_23 * m23 -
+                    (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23)),
             d *
-            (tmp_14 * m22 +
-                tmp_17 * m32 +
-                tmp_13 * m12 -
-                (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22)),
+                (tmp_14 * m22 +
+                    tmp_17 * m32 +
+                    tmp_13 * m12 -
+                    (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22)),
             d *
-            (tmp_20 * m32 +
-                tmp_12 * m02 +
-                tmp_19 * m22 -
-                (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02)),
+                (tmp_20 * m32 +
+                    tmp_12 * m02 +
+                    tmp_19 * m22 -
+                    (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02)),
             d *
-            (tmp_18 * m12 +
-                tmp_23 * m32 +
-                tmp_15 * m02 -
-                (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12)),
+                (tmp_18 * m12 +
+                    tmp_23 * m32 +
+                    tmp_15 * m02 -
+                    (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12)),
             d *
-            (tmp_22 * m22 +
-                tmp_16 * m02 +
-                tmp_21 * m12 -
-                (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02)),
+                (tmp_22 * m22 +
+                    tmp_16 * m02 +
+                    tmp_21 * m12 -
+                    (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02)),
         ];
     },
     lookAt: function (cameraPosition, target, up) {
@@ -307,4 +319,11 @@ function normalize(v) {
     } else {
         return [0, 0, 0];
     }
+}
+
+function normalVector(a, b, c) {
+    var v1 = subtractVectors(b, a);
+    var v2 = subtractVectors(c, a);
+    var normal = cross(v1, v2);
+    return normalize(normal);
 }
